e503a0424eb48928feefcbd2227fa8c6ea6a780a
Fletcher Blight <fletcherb@blackmagicdesign.com>
2022-07-15 15:55:20 +1000

    Now using buildenvs cpp-opengl

---
+++ config/conan.cmake
@@ -0,0 +1,20 @@
+set(CONAN_DIR "${CMAKE_CURRENT_BINARY_DIR}/deps")
+
+if(NOT EXISTS "${CONAN_DIR}/conan.cmake")
+    file(DOWNLOAD
+            "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
+            "${CONAN_DIR}/conan.cmake"
+            TLS_VERIFY ON)
+endif()
+include("${CONAN_DIR}/conan.cmake")
+
+set(ENV{CONAN_USER_HOME} "${CONAN_DIR}")
+conan_cmake_run(
+        CONANFILE config/conanfile.txt
+        BUILD missing
+        GENERATORS CMakeDeps
+        PROFILE default
+        PROFILE_AUTO ALL
+        INSTALL_FOLDER "${CONAN_DIR}")
+list(PREPEND CMAKE_MODULE_PATH "${CONAN_DIR}")
+list(PREPEND CMAKE_PREFIX_PATH "${CONAN_DIR}")
